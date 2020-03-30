a489858d256d847f7e88311cd8dcd27382efdf29
Dargons10 <jdamr2p@gmail.com>
2020-03-30 19:50:49 +0200

    mocha: update patches for P

--- patches/check.sh
+++ patches/check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="system/core frameworks/native external/selinux hardware/interfaces"
+dirs="system/core frameworks/native external/selinux"
 
 RED='\033[0;31m'
 NC='\033[0m'
