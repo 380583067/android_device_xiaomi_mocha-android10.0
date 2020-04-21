ef5fb6ca365b856ce005e7fc03ffdf51649efa05
Tommy Wu <wu.tommy@gmail.com>
2020-04-21 12:50:31 +0800

    mocha: bluetooth address 1

--- system_prop.mk
+++ system_prop.mk
@@ -2,6 +2,10 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.bt.enableAptXHD=true
 
+# BT
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/data/mocha_btmacaddr.txt
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
