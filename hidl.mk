3619066508c811a84211b3e3325e790b686de540
Dargons10 <jdamr2p@gmail.com>
2020-05-28 23:35:07 +0200

    mocha: Hidls updates

--- hidl.mk
+++ hidl.mk
@@ -75,6 +75,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service.mocha
