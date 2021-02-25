c644f39ce072a1be4b6d1c807fb5629165bb0deb
Dargons10 <jdamr2p@gmail.com>
2021-02-25 20:42:24 +0100

    mocha: Initial for Q

--- lineage_mocha.mk
+++ lineage_mocha.mk
@@ -2,9 +2,9 @@
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := aosp_mocha
+PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
 
