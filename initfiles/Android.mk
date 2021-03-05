5f64cde4bc724cacd56d7f001dfe5ce4640eac1a
Dargons10 <jdamr2p@gmail.com>
2021-03-05 22:24:37 +0100

    mocha: Libshim_zm called in independient file

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -120,3 +120,12 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := nvidia
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.nvgpu_shims.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.nvgpu_shims.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init
+include $(BUILD_PREBUILT)
+
