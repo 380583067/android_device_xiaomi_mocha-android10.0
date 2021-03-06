f1fdda6c0efeb8b7693707e008276f7801b2d1d9
Dargons10 <jdamr2p@gmail.com>
2021-03-06 22:15:53 +0100

    mocha: Fix shims for Q

--- shims/Android.mk
+++ shims/Android.mk
@@ -12,15 +12,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 ## liblog
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := stdio_vsnprintf.cpp
-LOCAL_C_INCLUDES := bionic/libc/stdio
-LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_MODULE := libs
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := stdio_vsnprintf.cpp
 LOCAL_C_INCLUDES := \
     bionic/libc \
