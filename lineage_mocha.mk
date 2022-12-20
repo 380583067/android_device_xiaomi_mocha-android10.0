f3d5e418b33256970121f6bb8742e7240197f753
380583067 <105837597+380583067@users.noreply.github.com>
2022-12-20 11:30:49 +0800

    Update lineage_mocha.mk
    for lineage

--- lineage_mocha.mk
+++ lineage_mocha.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := lineage_mocha
