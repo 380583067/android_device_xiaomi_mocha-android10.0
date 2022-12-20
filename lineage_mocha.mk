86277a60846000874a4740b186bf298bc45f1220
380583067 <105837597+380583067@users.noreply.github.com>
2022-12-20 11:20:31 +0800

    Update and rename aosp_mocha.mk to lineage_mocha.mk
    更新支持lineage

--- lineage_mocha.mk
+++ lineage_mocha.mk
@@ -5,9 +5,8 @@ $(call inherit-product, device/xiaomi/mocha/full_mocha.mk)
 $(call inherit-product, vendor/aosp/config/common_full_tablet_wifionly.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := aosp_mocha
+PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
 BOARD_VENDOR := Xiaomi
-
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
