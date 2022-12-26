9b243b160915a1c101f21836bfa0dfc6776b9103
Dargons10 <jdamr2p@gmail.com>
2022-12-26 16:29:07 +0100

    mocha: system_prop updated

--- system_prop.mk
+++ system_prop.mk
@@ -40,6 +40,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1
 
+#Dalvik
+RODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512 \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-threads=1
+
+
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
   persist.sys.usb.config=mtp
