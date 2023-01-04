d53a4f588d44eda6392f0528fde86b449c79764d
Dargons10 <jdamr2p@gmail.com>
2023-01-04 22:35:07 +0100

    mocha: Vendor true flags

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -18,6 +18,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.cal.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.cal.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -25,6 +26,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.comms.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.comms.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -32,6 +34,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.hdcp.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.hdcp.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -39,6 +42,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.t124.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.t124.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -53,6 +57,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tlk.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.tlk.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -60,6 +65,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tn8.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.tn8.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -67,6 +73,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tn8.usb.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.tn8.usb.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -74,6 +81,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tn8_common.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.tn8_common.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
@@ -81,6 +89,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.ussrd.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.ussrd.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
