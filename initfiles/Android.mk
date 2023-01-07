f5ffc053f46d552c0296232a6d4aba87253666cb
Dargons10 <jdamr2p@gmail.com>
2023-01-07 13:07:27 +0100

    mocha: disabled enctune to move at media folder

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -1,13 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := enctune.conf
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := enctune.conf
-LOCAL_MODULE_PATH  := $(TARGET_ETC_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.tn8
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.tn8
