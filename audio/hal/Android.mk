1ed0b597795e8fe5268e43eed43f6f33aed3cbcd
Dargons10 <jdamr2p@gmail.com>
2023-01-07 18:55:52 +0100

    mocha: Added audio hal

--- audio/hal/Android.mk
+++ audio/hal/Android.mk
@@ -19,30 +19,52 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libaudiohalcm
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+ifeq ($(strip $(BOARD_USES_TINYHAL_AUDIO)),true)
+include $(CLEAR_VARS)
+
+ifeq ($(strip $(TINYHAL_AUDIO_MODULE_NAME)),)
+LOCAL_MODULE := audio.primary.$(TARGET_BOARD_PLATFORM)
+else
+LOCAL_MODULE := audio.$(strip $(TINYHAL_AUDIO_MODULE_NAME)).$(TARGET_DEVICE)
+endif
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_CFLAGS += -Werror -Wno-error=unused-parameter -Wno-unused-parameter
 
 LOCAL_C_INCLUDES += \
 	external/tinycompress/include \
-	external/tinyhal/include \
 	device/xiaomi/mocha/tinyalsa/include \
+	external/tinyhal/include \
 	external/expat/lib \
 	$(call include-path-for, audio-utils)
 
 LOCAL_SRC_FILES := \
-	audio_config.c
+	audio_hw.c
+
+LOCAL_STATIC_LIBRARIES := \
+	libmedia_helper
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
 	libutils \
 	libdl	\
 	liblog	\
-	libexpat	\
+	libhardware_legacy \
 	libtinyalsa_mocha	\
+	libtinycompress	\
+	libaudiohalcm \
+	libaudioutils \
+	libsysutils
+
+ifeq ($(strip $(VOICE_RECOGNITION_REQUIRES_UNSHORTEN)),true)
+LOCAL_C_INCLUDES += vendor/wolfson/tools/libs/libunshorten/include
+LOCAL_CFLAGS += -DCOMPRESS_PCM_USE_UNSHORTEN
+LOCAL_SHARED_LIBRARIES += libunshorten
+endif
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
 
