15f18dc23e654084eb77cb772de58ad6f57b678d
Dargons10 <jdamr2p@gmail.com>
2023-01-11 20:26:57 +0100

    mocha: Added blueprint

--- lineage_mocha.mk
+++ lineage_mocha.mk
@@ -6,6 +6,13 @@ $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_mocha
 PRODUCT_DEVICE := mocha
+PRODUCT_BAND := xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
 BOARD_VENDOR := Xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Xiaomi/carbon_mocha/mocha:5.1.1/LMY49J/7fd38a3d2b:user/release-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
+  
