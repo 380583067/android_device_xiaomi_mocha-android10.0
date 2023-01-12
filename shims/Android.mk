553461ec46e3313e1a73646de34b0d52b92eefe4
Dargons10 <jdamr2p@gmail.com>
2023-01-12 21:09:26 +0100

    Add protobuf 3.9.1 symlink

--- shims/Android.mk
+++ shims/Android.mk
@@ -3,6 +3,15 @@ LOCAL_PATH:= $(call my-dir)
 ## libshim_atomic
 include $(CLEAR_VARS)
 
+PROTOBUF_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libprotobuf-cpp-lite-3.9.1.so
+$(PROTOBUF_SYMLINK):
+	$(hide) mkdir -p $(TARGET_OUT_VENDOR)/lib
+	$(hide) ln -sf /system/lib/libprotobuf-cpp-lite.so $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(PROTOBUF_SYMLINK)
+
+
+
 LOCAL_SRC_FILES := atomic.cpp
 LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
