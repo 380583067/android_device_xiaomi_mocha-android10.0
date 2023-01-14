b5e3d89da42c32fab7d40185e136068776ece4ac
Dargons10 <jdamr2p@gmail.com>
2023-01-14 15:04:15 +0100

    move to vendor for init.tegra.rc

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -43,6 +43,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.tegra.rc
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.tegra.rc
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
