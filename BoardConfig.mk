a284fc0e1f80282dd197c7c074c78f885cadec09
Dargons10 <jdamr2p@gmail.com>
2023-01-18 21:58:52 +0100

    Override vmalloc to 450MB

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 EXTENDED_FONT_FOOTPRINT := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := vpr_resize androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := vpr_resize androidboot.selinux=permissive vmalloc=450M
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
