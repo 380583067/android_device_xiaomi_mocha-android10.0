458209f3293ee50495de946673cd865393200362
Dargons10 <jdamr2p@gmail.com>
2023-01-31 21:11:10 +0100

    Add davilk to system

--- system_prop.mk
+++ system_prop.mk
@@ -13,6 +13,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.btmacaddr=00:00:00:00:00:00
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=2m \
+    dalvik.vm.heapmaxfree=8m
+
 #DIDIM
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.tegra.didim.enable = 1 \
