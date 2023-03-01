c42ac941fa3c72ef9de5a2d35745e7a8fc97d6ba
dargons10 <jdamr2p@gmail.com>
2023-03-01 14:53:35 +0100

    Revert commit aacf1314

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -107,6 +107,14 @@ LOCAL_SRC_FILES    := ueventd.tn8.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE               := init.nvgpu_shims.rc
+LOCAL_MODULE_CLASS         := ETC
+LOCAL_SRC_FILES            := init.nvgpu_shims.rc
+LOCAL_VENDOR_MODULE        := true
+LOCAL_MODULE_RELATIVE_PATH := init
+include $(BUILD_PREBUILT)
+
 
 
 
