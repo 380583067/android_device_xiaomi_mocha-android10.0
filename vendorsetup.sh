1da8d6212f41d6ea19b3640ed39adb2b4f7ed824
apple <105837597+380583067@users.noreply.github.com>
2023-07-15 13:55:30 +0000

    Delete vendorsetup.sh

--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-# Create ld symlink to avoid linking error(s)
-ln -s $(pwd)/prebuilts/clang/host/linux-x86/clang-r383902b/bin/lld \
-    $(pwd)/prebuilts/clang/host/linux-x86/clang-r383902b/bin/ld &> /dev/null
