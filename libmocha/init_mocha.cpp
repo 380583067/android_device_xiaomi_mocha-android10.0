e74544591bf0f725d7709aa496da814c907a78a1
apple <105837597+380583067@users.noreply.github.com>
2023-07-15 16:40:03 +0000

    Update init_mocha.cpp
    ld.lld: error: undefined symbol: vendor_load_properties()
    >>> referenced by property_service.cpp:917 (system/core/init/property_service.cpp:917)
    >>>               property_service.o:(android::init::PropertyLoadBootDefaults()) in archive out/soong/.intermediates/system/core/init/libinit/android_recovery_arm_armv7-a-neon_cortex-a15_static/libinit.a
    clang-11: error: linker command failed with exit code 1 (use -v to see invocation)

--- libmocha/init_mocha.cpp
+++ libmocha/init_mocha.cpp
@@ -47,3 +47,8 @@ void vendor_load_properties()
     android::init::property_set("dalvik.vm.heapminfree", heapminfree);
     android::init::property_set("dalvik.vm.heapmaxfree", "8m");
 }
+
+void vendor_load_properties()
+{
+
+}
